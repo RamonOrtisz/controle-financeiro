dbd96ecbb918f163d5e6e9a6c3344ee09bb81f7e
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-11-24 22:16:09 -0300

    header create

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <header className="bg-teal-600 h-40">
+            <div className="flex justify-center items-center h-full">
+                <h1 className="text-4xl font-bold text-white">Controle Financeiro</h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header
